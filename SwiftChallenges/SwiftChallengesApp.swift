7ca6d0adb1c3a19bad35b11106d8388c7c4a9b14
GEORGE QUENTIN <ggeoo93@gmail.com>
2023-03-11 18:17:17 +0000

    new project

---
+++ SwiftChallenges/SwiftChallengesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChallengesApp.swift
+//  SwiftChallenges
+//
+//  Created by GEORGE QUENTIN on 11/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChallengesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
